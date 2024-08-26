98762fb0d22d0abd6624d05d99ecdef113e6cafd
taabojo0 (local) <taabojo0@um00764.home>
2024-08-26 22:27:29 +0200

    adding lazygit and justifle
    Adding lazygit for better git integration
    Adding justfile support to automate repos

--- lua/jo/plugins/init.lua
+++ lua/jo/plugins/init.lua
@@ -1,9 +1,6 @@
 return {
   -- all plugins which need no setup belongs here
-
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
+  'echasnovski/mini.nvim',
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
