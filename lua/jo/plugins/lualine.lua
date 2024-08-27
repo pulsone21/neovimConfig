8369c6e14726ebf157a81972de1f5dd531fdc7a6
Johannes Böhm <pulsone21@freenet.de>
2024-08-27 21:18:05 +0200

    Improving lua line

--- lua/jo/plugins/lualine.lua
+++ lua/jo/plugins/lualine.lua
@@ -1,13 +1,22 @@
 return {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'auto',
-        component_separators = '|',
-        section_separators = '',
-      },
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = 'auto',
+      section_separators = { left = '', right = '' },
+      component_separators = { left = '', right = '' },
     },
-  }
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { 'filename' },
+      lualine_x = { { 'fileformat', symbols = { unix = '' } }, 'filetype' },
+      lualine_y = { 'swenv' },
+      lualine_z = { 'location' },
+    },
+  },
+}
